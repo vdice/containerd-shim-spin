96e915d876473dc08c6e2c7e430bbe685014d417
Kate Goldenring <kate.goldenring@gmail.com>
2025-06-24 18:31:00 +0000

    Update to use Spin v3.3.1 dependencies
    Signed-off-by: Kate Goldenring <kate.goldenring@gmail.com>

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y protobuf-compiler libseccomp-dev
 
 ## setup tinygo. required for building test spin app
 echo "setting up tinygo"
-wget https://github.com/tinygo-org/tinygo/releases/download/v0.34.0/tinygo_0.34.0_amd64.deb
-sudo dpkg -i tinygo_0.34.0_amd64.deb
-rm tinygo_0.34.0_amd64.deb
+wget https://github.com/tinygo-org/tinygo/releases/download/v0.38.0/tinygo_0.38.0_amd64.deb
+sudo dpkg -i tinygo_0.38.0_amd64.deb
+rm tinygo_0.38.0_amd64.deb
